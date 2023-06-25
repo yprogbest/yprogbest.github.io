34de3b10b18662fb36a362d96d89046777b3baa1
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-25 10:19:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function showFullArticle(event) {
   var button = event.target;
-  var fullArticle = button.nextElementSibling;
-  
+  var article = button.parentNode;
+  var fullArticle = article.querySelector('.full-article');
+
   fullArticle.classList.remove('hidden');
+  fullArticle.style.display = 'block';
   button.style.display = 'none';
 }
