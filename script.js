2c841696317e2681f31f60ed9c9ea67718a2a6f3
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-25 11:29:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function showFullArticle(event, articleId) {
   var button = event.target;
   var article = button.parentNode;
-  var fullArticle = article.querySelector('.full-article');
+  var fullArticle = article.querySelector('#' + articleId);
 
-  fullArticle.classList.remove('hidden');
-  fullArticle.style.display = 'block';
-  button.style.display = 'none';
+  if (fullArticle.classList.contains('hidden')) {
+    fullArticle.classList.remove('hidden');
+    button.textContent = '閉じる';  // ボタンのテキストを変更するなど、必要に応じて追加の処理を行う
+  } else {
+    fullArticle.classList.add('hidden');
+    button.textContent = '続きを見る';
+  }
 }
 
