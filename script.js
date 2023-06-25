5ac48d470ed517e04ae33192beb49865f858c0b5
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-25 10:45:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function showFullArticle(event, articleId) {
   var fullArticle = article.querySelector('.full-article');
 
   fullArticle.classList.remove('hidden');
+  fullArticle.style.display = 'block';
   button.style.display = 'none';
 }
