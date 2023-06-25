5fdb7448293dd427c49d63018e9fb4487b339dc1
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-25 10:53:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function showFullArticle(event) {
 
   if (fullArticle.classList.contains('hidden')) {
     fullArticle.classList.remove('hidden');
+    fullArticle.style.display = 'block';
     button.textContent = '閉じる';  // ボタンのテキストを変更するなど、必要に応じて追加の処理を行う
   } else {
     fullArticle.classList.add('hidden');
