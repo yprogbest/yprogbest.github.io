935010ad48bdbd6719690c6215a80e92894034ab
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-25 11:00:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ function showFullArticle(event) {
 
   if (fullArticle.classList.contains('hidden')) {
     fullArticle.classList.remove('hidden');
-    fullArticle.style.display = 'block';
     button.textContent = '閉じる';  // ボタンのテキストを変更するなど、必要に応じて追加の処理を行う
   } else {
     fullArticle.classList.add('hidden');
-    fullArticle.style.display = 'none';
     button.textContent = '続きを見る';
   }
 }
