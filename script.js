313118462d49327233ff53a1332f5e80e6b24338
YProg <82224433+yprogbest@users.noreply.github.com>
2023-06-27 20:20:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function scrollArticle(event){
     behavior: "smooth",
   });
 }
+
+
+
+//H1タグのレイアウト（ネオン管風）
+function h1Layout(){
+  
+  const target = window.document.getElementsByTagName('h1')[0]
+  
+  const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+  const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+  const flickerAndColorText = text => 
+    text
+      .split('')
+      .map(flickerLetter)
+      .map(colorLetter)
+      .join('');
+  const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
+  
+  neonGlory(target);
+  target.onclick = ({ target }) =>  neonGlory(target);
+}
+
