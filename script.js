6e0e0ba20b5637e3b8091b4390c6b12c21c25f89
YProg <82224433+yprogbest@users.noreply.github.com>
2023-07-12 15:29:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// 各記事の「続きを見る」ボタンの処理
-function showFullArticle(event, articleId) {
-  var button = event.target;
-  var fullArticle = document.querySelector('#' + articleId);
-
-  if (fullArticle.classList.contains('hidden')) {
-    fullArticle.classList.remove('hidden');
-    button.innerText = '閉じる';  // ボタンのテキストを変更するなど、必要に応じて追加の処理を行う
-  } else {
-    fullArticle.classList.add('hidden');
-    button.innerText = '続きを見る';
-  }
-}
-
-
 // スクロール用のボタンの処理
 function scrollArticle(event){
   
