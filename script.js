bb24247dd6b3a64e795750a23d1e5ce377762624
YProg <82224433+yprogbest@users.noreply.github.com>
2023-07-29 17:35:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function scrollArticle(event){
     behavior: "smooth",
   });
 }
+
+
+// おすすめ記事TOP3の処理
+const swiper = new Swiper(".swiper", {
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "2",
+
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true
+  },
+
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
