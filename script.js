57811afe0b6502a93bc2d3cc7e1580358dce2d35
YProg <82224433+yprogbest@users.noreply.github.com>
2023-07-29 18:19:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: "1.3",
+  slidesPerView: "1.5",
 
   coverflowEffect: {
     rotate: 50,
